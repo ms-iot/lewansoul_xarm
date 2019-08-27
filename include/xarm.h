7f95074ba2f261e9a4824ea9022f043fb2c7549b
PhDN <phuongnguyening@gmail.com>
2019-08-27 01:43:20 -0700

    updated xarm.h

---
+++ include/xarm.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+// platform
+#include <windows.h>
+
+namespace xArm {
+
+    class xArm
+    {
+        public:
+        xArm();
+        ~xArm();
+        struct Joints 
+        {
+            Joints()
+            {
+                data.resize(6, -1);
+            }
+            std::vector<double> data;
+        };
+        std::vector<double> convertToRadian(const std::vector<int>& servoReadings);
+
+        std::vector<int> convertToServoReadings(const std::vector<double>& radian);
+
+        void MoveToPosition(::HANDLE device, const Joints& pos);
+
+        Joints readPosition(::HANDLE device);
+
+        void PrintCurrentPosition(::HANDLE device);
+    };
+
+} // namespace xarm
